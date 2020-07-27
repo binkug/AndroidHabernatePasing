c9837e9405c5379a2ef6865398852708da4447a5
binkug <binkug@naver.com>
2020-07-27 17:34:44 +0900

    Initial commit

---
+++ app/src/main/java/com/example/androidportpolio/ActivitiyDetailActivity.java
@@ -0,0 +1,175 @@
+package com.example.androidportpolio;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ActivitiyDetailActivity extends AppCompatActivity {
+    //화면에 사용할 뷰
+    TextView activity_subject,activity_content,activity_type;
+    ImageView activity_image;
+
+    //텍스트 데이터를 웹에서 다운로드 받아서 출력
+    //다운로드 -> 파싱 -> 출력
+    //다운로드는 스레드를 이용 출력은 핸들러를 이용해야 한다.
+
+    //이미지 출력을 위한 핸들러
+    Handler handlerImage = new Handler(Looper.getMainLooper()){
+        @Override
+        public void handleMessage(Message msg) {
+
+            //스레드가 전달해준 데이터를 이미지 뷰에 출력
+            Bitmap bitmap = (Bitmap) msg.obj;
+            activity_image.setImageBitmap(bitmap);
+        }
+    };
+    //이미지 다운로드를 위한 스레드
+    class ImageThread extends Thread{
+        String subject_image;
+
+        public ImageThread(String subject_image){
+            this.subject_image  = subject_image;
+        }
+
+        @Override
+        public void run() {
+            try {
+                URL url = new URL("http://192.168.0.117:9000/img/"+subject_image);
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setUseCaches(false);
+                con.setConnectTimeout(30000);
+
+                //이미지 출력
+                InputStream is = url.openStream();
+                Bitmap bitmap = BitmapFactory.decodeStream(is);
+                //Message에 저장
+                Message message = new Message();
+                message.obj = bitmap;
+                handlerImage.sendMessage(message);
+            }catch (Exception e){
+                Log.e("이미지 다운로드 에러",e.getMessage());
+            }
+        }
+    }
+
+
+    //텍스트 데이터를 출력할 핸들러
+    Handler handler = new Handler(Looper.getMainLooper()){
+        @Override
+        public void handleMessage(Message msg) {
+            //넘어온 데이터 찾아오기
+            Map<String,Object> map = (Map<String, Object>) msg.obj;
+            //데이터 출력하기
+            activity_subject.setText((String) map.get("activity_subject"));
+            activity_content.setText((String) map.get("activity_content"));
+            activity_type.setText((String) map.get("activity_type"));
+            //이미지 파일명을 ImageThread에게 넘겨서 출력
+            new ImageThread((String) map.get("activity_image")).start();
+
+
+        }
+    };
+
+
+    //텍스트 데이터를 가져올 스레드 클래스
+    class ThreadEx extends Thread{
+        @Override
+        public void run() {
+            //텍스트 데이터 다운로드
+            StringBuilder sb = new StringBuilder();
+            try {
+                //URL 만들기
+                URL url = new URL("http://192.168.0.117:9000/activity/detail?activity_num="+6);
+
+                //Connection 객체 만들기
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setConnectTimeout(3000);
+                con.setUseCaches(false);
+
+                //스트림 객체 생성
+                BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+
+                //문자열 읽기
+                while(true){
+                    String line = br.readLine();
+                    if(line == null){
+                        break;
+                    }
+                    sb.append(line+"\n");
+                }
+                br.close();
+                con.disconnect();
+
+            }catch (Exception e){
+                //이 메세지가 보이면 서버가 구동중인지 확인하고 URL은 제대로 입력했는지 확인
+                Log.e("다운로드 에러",e.getMessage());
+            }
+            //데이터를 다운로드 받았는지 확인 할 수 있는 코드
+            Log.e("다운로드 받은 문자열",sb.toString());
+
+            try {
+                //다운로드 받은 문자열에서 필요한 데이터 추출
+                JSONObject object = new JSONObject(sb.toString());
+                JSONObject activity = object.getJSONObject("activity");
+
+                String activity_subject = activity.getString("activity_subject");
+                String activity_type = activity.getString("activity_type");
+                String activity_content = activity.getString("activity_content");
+                String activity_image = activity.getString("activity_image");
+
+                //4개의 데이터를 하나로 묶기
+                Map<String,Object> map = new HashMap<String, Object>();
+                map.put("activity_subject",activity_subject);
+                map.put("activity_type",activity_type);
+                map.put("activity_content",activity_content);
+                map.put("activity_image",activity_image);
+
+                //핸들러에게 데이터를 전송하고 호출
+                Message message = new Message();
+                message.obj = map;
+                handler.sendMessage(message);
+
+            }catch (Exception e){
+                Log.e("파싱 에러",e.getMessage());
+            }
+        }
+    }
+
+    //확인용 메소드
+    @Override
+    public void onResume(){
+        super.onResume();
+        new ThreadEx().start();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_activitiy_detail);
+
+        //디자인 한 뷰 찾아오기
+        activity_subject = (TextView)findViewById(R.id.activity_subject);
+        activity_content = (TextView)findViewById(R.id.activity_content);
+        activity_type = (TextView)findViewById(R.id.activity_type);
+        activity_image = (ImageView) findViewById(R.id.activity_image);
+    }
+}
---
+++ app/src/main/java/com/example/androidportpolio/InsertActivity.java
@@ -0,0 +1,178 @@
+package com.example.androidportpolio;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.os.Message;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.UUID;
+
+public class InsertActivity extends AppCompatActivity {
+    EditText activity_subject,activity_type,activity_content;
+    Button btninsert;
+
+
+
+    class InsertThread extends Thread{
+        @Override
+        public void run() {
+
+            try {
+                //upload할 주소 만들기
+                URL url = new URL("http://192.168.0.117:9000/activity/insert");
+                //서버에게 넘겨줄 문자열 파라미터 생성
+                String user_email = "PncQZK8LD1Lf+/LlORX+haAqcb9DeZQjrPRnoJYr8Skuf9FVm8LIqfTClXYdPeYXz6ndcI67b8vyW8ATC9uvH/NZ7U8=";
+                String [] data = {user_email,activity_subject.getText().toString().trim(),activity_type.getText().toString().trim(),activity_content.getText().toString().trim()};
+                String [] dataName = {"user_email","activity_subject","activity_type","activity_content"};
+
+                Log.e("user_email",user_email);
+                Log.e("activity_subject",activity_subject.getText().toString().trim());
+                Log.e("activity_type",activity_type.getText().toString().trim());
+                Log.e("activity_content",activity_content.getText().toString().trim());
+
+                //파라미터 전송에 필요한 변수를 생성
+                String lineEnd = "\r\n";
+                //파일 업로드를 할 때는 boundary 값이 있어야 합니다.
+                //랜덤하게 생성하는 것을 권장
+                String boundary = UUID.randomUUID().toString();
+
+                //업로드 옵션을 설정
+                HttpURLConnection con = (HttpURLConnection) url.openConnection();
+                con.setRequestMethod("POST");
+                con.setConnectTimeout(30000);
+                con.setUseCaches(false);
+                con.setDoInput(true);
+                con.setDoOutput(true);
+
+                //파일 업로드 옵션 설정
+                con.setRequestProperty("ENTYPE","multipart/form-data");
+                con.setRequestProperty("Content-Type","multipart/form-data;boundary="+boundary);
+
+                //문자열 파라미터를 전송
+                String delimiter = "--"+boundary+lineEnd;
+                StringBuffer postBataBuiler = new StringBuffer();
+                for(int i=0;i<data.length;i=i+1){
+                    postBataBuiler.append(delimiter);
+                    postBataBuiler.append("Content-Disposition: form-data; name=\""+dataName[i]+"\""+ lineEnd + lineEnd + data[i] + lineEnd);
+                }
+
+                //업로드할 파일이 있는 경우에만 작성
+                String fileName = "ball.png";
+                if(fileName !=null){
+                    postBataBuiler.append(delimiter);
+                    postBataBuiler.append("Content-Disposition: form-data; name=\""+"activity_image"+"\";filename=\""+fileName+"\""+lineEnd);
+                }
+                //파라미터 전송
+                DataOutputStream ds = new DataOutputStream(con.getOutputStream());
+                ds.write(postBataBuiler.toString().getBytes());
+
+                //파일 업로드
+                if(fileName != null){
+                    ds.writeBytes(lineEnd);
+                    //파일 읽어오기 -id에 해당하는 파일을 raw 디렉토리에 복사
+                    InputStream fres = getResources().openRawResource(R.raw.ball);
+                    byte [] buffer = new byte[fres.available()];
+                    int length = -1;
+                    //파일의 내용을 읽어서 읽은 내용이 있으면 그 내용을 ds에 기록
+                    while ((length = fres.read(buffer)) != -1){
+                        ds.write(buffer,0,length);
+                    }
+                    ds.writeBytes(lineEnd);
+                    ds.writeBytes(lineEnd);
+                    ds.writeBytes("--"+boundary+"--"+lineEnd);
+                    fres.close();
+                }else{
+                    ds.writeBytes(lineEnd);
+                    ds.writeBytes("--"+boundary+"--"+lineEnd);
+                }
+                ds.flush();
+                ds.close();
+
+                //서버로 부터의 응답 가져오기
+                BufferedReader br = new BufferedReader(new InputStreamReader(con.getInputStream()));
+                StringBuilder sb = new StringBuilder();
+                while(true){
+                    String line = br.readLine();
+                    if(line == null){
+                        break;
+                    }
+                    sb.append(line + "\n");
+                }
+                br.close();
+                con.disconnect();
+
+                //JSON 파싱
+                JSONObject object = new JSONObject(sb.toString());
+                boolean insert = object.getBoolean("insert");
+                //핸들러에게 전송
+                Message message = new Message();
+                message.obj = insert;
+                insertHandler.sendMessage(message);
+
+            }catch (Exception e){
+                Log.e("업로드 에러 에러",e.getMessage());
+            }
+
+        }
+    }
+
+    Handler insertHandler = new Handler(Looper.getMainLooper()){
+        @Override
+        public void handleMessage(Message msg) {
+            boolean insert = (boolean) msg.obj;
+            if(insert == true){
+                Toast.makeText(InsertActivity.this,"삽입 성공",Toast.LENGTH_LONG).show();
+                //키보드 내리기
+                InputMethodManager imm = (InputMethodManager)getSystemService(INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(activity_subject.getWindowToken(),0);
+                imm.hideSoftInputFromWindow(activity_type.getWindowToken(),0);
+                imm.hideSoftInputFromWindow(activity_content.getWindowToken(),0);
+            }else{
+                Toast.makeText(InsertActivity.this,"삽입 실패",Toast.LENGTH_LONG).show();
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insert);
+        activity_subject = (EditText)findViewById(R.id.activity_subject);
+        activity_type = (EditText)findViewById(R.id.activity_type);
+        activity_content = (EditText)findViewById(R.id.activity_content);
+        btninsert = (Button) findViewById(R.id.btninsert);
+
+        btninsert.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //유효성 검사
+                if(activity_subject.getText().toString().trim().length() < 1){
+                    Toast.makeText(InsertActivity.this,"제목을 입력해 주세요",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(activity_type.getText().toString().trim().length() < 1){
+                    Toast.makeText(InsertActivity.this,"타입을 입력해 주세요",Toast.LENGTH_LONG).show();
+                    return;
+                }
+                new InsertThread().start();
+            }
+        });
+    }
+}
